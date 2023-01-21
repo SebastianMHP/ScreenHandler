68b722477e89541e0365b5f6f779cd195639a0c2
SebastianMHP <sebastianhenriquez43@gmail.com>
2023-01-20 23:02:40 -0400

    Agregar Proyecto

---
+++ CRUDJSON/Menu.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDJSON
+{
+    public class Menu:Program
+    {
+        string op_menu = "";
+        public void iniciar()
+        {
+            do
+            {
+                cabecera();
+
+            } while (op_menu != "0");
+        }
+        private void cabecera()
+        {
+            Console.WriteLine("|-------------WELCOME Screen #1----------|");
+            Console.WriteLine("|-------------------Menu-----------------|");
+            Console.WriteLine("|1 [Crear Registro]   | 3 [Eliminar]     |");
+            Console.WriteLine("|2 [Listar Registro]  | 4 [Actualizar]   |");
+            Console.WriteLine("|5 [Buscar]           | 0 [Salir]        |");
+            Console.WriteLine("|----------------------------------------|");
+            Console.WriteLine("\n");
+            Console.WriteLine("Digite su opcion:");
+            op_menu = Console.ReadLine();
+            selecMenu(op_menu);
+        }
+        private void selecMenu(string op)
+        {
+            if (op == "")
+                return;
+            switch (op)
+            {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("--WELCOME Screen Registrarse---");
+                    Agregarperson();
+                    volver_menu();
+                    Console.ReadKey();
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("--WELCOME Screen Datos---");
+                    MostrarPerson();
+                    volver_menu();
+                    Console.ReadKey();
+                    break;
+                case "3":
+                    Console.Clear();
+                    Console.WriteLine("--WELCOME Screen Eliminar---");
+                    EliminarPerson();
+                    volver_menu();
+                    Console.ReadKey();
+                    break;
+                case "4":
+                    Console.Clear();
+                    Console.WriteLine("--WELCOME Screen Actualizar---");
+                    ActualizarPerson();
+                    volver_menu();
+                    Console.ReadKey();
+                    break;
+                case "5":
+                    Console.Clear();
+                    Console.WriteLine("--WELCOME Screen Buscar---");
+                    buscar();
+                    volver_menu();
+                    Console.ReadKey();
+                    break;
+                case "q":
+                    Console.Clear();
+                    cabecera();
+                    Console.ReadKey();
+                    break;
+                case "0":
+                    Console.Clear();
+                    Console.WriteLine("Screen Exit");
+                    Console.WriteLine("-----------BYE-----------------");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("opcion incorrecta");
+                    Console.ReadKey();
+                    Console.Clear ();
+                    cabecera ();
+
+                    break;
+            }
+        }
+        private void volver_menu()
+        {
+            string op;
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Digite (q) para volver al menu");
+
+            op = Console.ReadLine();
+            selecMenu(op);
+        }
+        /*
+        private bool lista_vacia()
+        {
+            
+            if (jsonFile.ToString())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }*/
+    }
+}
---
+++ CRUDJSON/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;  
+
+
+namespace CRUDJSON
+{
+    
+    public class Program
+    {
+        public string jsonFile = @"C:\Users\pjuli\source\repos\CRUDJSON\CRUDJSON\user.json";
+        public void Agregarperson()  
+        {  
+            Console.WriteLine("Digita el ID: ");  
+            var studentid = Console.ReadLine();  
+            Console.WriteLine("\nDigita su nombre: ");
+            var studentname = Console.ReadLine();  
+  
+            var newStudent = "{ 'studentid': " + studentid + ", 'studentname': '" + studentname + "'}";  
+            try  
+            {  
+                var json = File.ReadAllText(jsonFile);  
+                var jsonObj = JObject.Parse(json);  
+                var studentsArrary = jsonObj.GetValue("students") as JArray;  
+                var newStud = JObject.Parse(newStudent);  
+                studentsArrary.Add(newStud);  
+
+                 jsonObj["students"] = studentsArrary;  
+                string newJsonResult = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);  
+                File.WriteAllText(jsonFile, newJsonResult);
+                Console.Write("\n");
+                Console.WriteLine("Informacion guardada");
+            }  
+            catch (Exception ex)  
+            {  
+                Console.WriteLine("Add Error : " + ex.Message.ToString());  
+            }  
+        }  
+        public void ActualizarPerson()  
+        {  
+            string json = File.ReadAllText(jsonFile);  
+  
+            try  
+            {  
+                var jObject = JObject.Parse(json);  
+                JArray studentsArrary = (JArray)jObject["students"];  
+                Console.Write("Digita el id para actualizar/modificar : ");  
+                var studentId = Convert.ToInt32(Console.ReadLine());
+                
+                if (studentId > 0)  
+                {  
+                    Console.Write("Digita su nombre : ");  
+                    var studentName = Convert.ToString(Console.ReadLine());  
+  
+                    foreach (var student in studentsArrary.Where(obj => obj["studentid"].Value<int>() == studentId))  
+                    {  
+
+                        student["studentname"] = !string.IsNullOrEmpty(studentName) ? studentName : "";  
+                    }  
+  
+                    jObject["students"] = studentsArrary;  
+                    string output = Newtonsoft.Json.JsonConvert.SerializeObject(jObject, Newtonsoft.Json.Formatting.Indented);  
+                    File.WriteAllText(jsonFile, output);
+                    Console.Write("\n");
+                    Console.WriteLine("Datos actualizados");
+                }  
+                else  
+                {  
+                    Console.Write("Id invalido");  
+                    ActualizarPerson();  
+                }  
+            }  
+            catch (Exception ex)  
+            {  
+  
+                Console.WriteLine("Update Error : " + ex.Message.ToString());  
+            }  
+        }  
+
+        public void EliminarPerson()  
+        {  
+            var json = File.ReadAllText(jsonFile);
+            var jObject = JObject.Parse(json);
+            JArray studentsArrary = (JArray)jObject["students"];
+            if (lista_vacia() == true)
+            {
+                Console.WriteLine("La lista esta vacia");
+            }
+            else
+            {
+                Console.Write("Digita el id para eliminar : ");
+                var studentId = Convert.ToInt32(Console.ReadLine());
+
+                var studentName = string.Empty;
+                var studentToDeleted = studentsArrary.FirstOrDefault(obj => obj["studentid"].Value<int>() == studentId);
+
+                studentsArrary.Remove(studentToDeleted);
+
+                string output = Newtonsoft.Json.JsonConvert.SerializeObject(jObject, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(jsonFile, output);
+                Console.WriteLine("Dato eliminado");
+            }
+ 
+        }
+        public bool lista_vacia()
+        {
+            var json = File.ReadAllText(jsonFile);
+            var jObject = JObject.Parse(json);
+            if (jObject.Count==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void MostrarPerson()  
+        {
+            var json = File.ReadAllText(jsonFile);
+            var jObject = JObject.Parse(json);
+            if (lista_vacia()==true)
+            {
+                Console.WriteLine("La lista esta vacia");
+            }
+            else
+            {
+                Console.WriteLine("|----Lista---------------------|");
+                Console.WriteLine("| ID :" + jObject["id"].ToString());
+                Console.WriteLine("| Universidad :" + jObject["Universidad"].ToString());
+
+                var Dirreccion = jObject["Dirreccion"];
+                Console.WriteLine("| Calle :" + Dirreccion["Calle"].ToString());
+                Console.WriteLine("| Ciudad :" + Dirreccion["Ciudad"].ToString());
+                Console.WriteLine("| codigoPostal :" + Dirreccion["codigoPostal"]);
+                Console.WriteLine("|------Estudiantes: -----------");
+                JArray studentsArrary = (JArray)jObject["students"];
+                if (studentsArrary != null)
+                {
+                    foreach (var item in studentsArrary)
+                    {
+                        Console.WriteLine("| Id : {0} | nombre : {1} ", item["studentid"], item["studentname"]);
+                        //Console.WriteLine("Id :" + item["studentid"]);
+                        //Console.WriteLine("Name :" + item["studentname"].ToString());
+                    }
+
+                }
+            }
+            
+        }
+        public class Dirreccion
+        {
+            public string Calle { get; set; }
+            public string Ciudad { get; set; }
+            public int codigoPostal { get; set; }
+        }
+
+        public class Root
+        {
+            public int id { get; set; }
+            public string Universidad { get; set; }
+            public Dirreccion Dirreccion { get; set; }
+            public List<Student> students { get; set; }
+        }
+
+        public class Student
+        {
+            public int studentid { get; set; }
+            public string studentname { get; set; }
+        }
+        public void buscar()
+        {
+            if (lista_vacia() == true)
+            {
+                Console.WriteLine("La lista esta vacia");
+            }
+            else
+            {
+                int buscar;
+                string link = @"C:\Users\pjuli\source\repos\CRUDJSON\CRUDJSON\user.json";
+                WebRequest request = WebRequest.Create(link);
+                WebResponse response = request.GetResponse();
+                Console.WriteLine("Digite su id para buscarlo: ");
+                buscar=Convert.ToInt32(Console.ReadLine());
+               
+
+                using (Stream datastream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(datastream);
+                    string responsefromserver = reader.ReadToEnd();
+
+                    Root root = JsonConvert.DeserializeObject<Root>(responsefromserver);
+
+                    foreach (Student s in root.students)
+                    {
+                        if (buscar ==s.studentid)
+                        {
+                            Console.WriteLine("-----Dato encontrado: ---");
+                            Console.Write("\n");
+                            Console.WriteLine("| Id : {0} | nombre : {1} ", s.studentid, s.studentname);
+                            //Console.WriteLine("id= " + s.studentid + " Name: " + s.studentname);
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                        }
+                        
+                    }
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program objProgram = new CRUDJSON.Program();
+
+            Menu start = new Menu();
+            start.iniciar();
+
+        }  
+       
+    }
+}
